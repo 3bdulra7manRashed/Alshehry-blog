e2791a4a9e749c82fe538fae8b6247ec233419b1
3bdulra7manRashed <3bdulra7manra4ed@gmail.com>
2026-01-18 22:42:39 +0200

    update SEO

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule sitemap generation daily at 6 AM
+Schedule::command('sitemap:generate')->dailyAt('06:00')
+    ->appendOutputTo(storage_path('logs/sitemap.log'))
+    ->description('Generate sitemap.xml for SEO');
