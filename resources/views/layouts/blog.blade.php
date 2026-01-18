26835a8aca67a117abe431b627a446b429ac387d
3bdulra7manRashed <3bdulra7manra4ed@gmail.com>
2026-01-18 22:47:25 +0200

    add to google consol

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -8,6 +8,9 @@
     <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
     <meta name="theme-color" content="#c37c54">
     <meta name="googlebot" content="index, follow">
+    
+    {{-- Google Search Console Verification --}}
+    <meta name="google-site-verification" content="dsFJffJ4xhPZeS16nSG0npAk5pKhQVwDd9KTCPn2N34" />
 
     {{-- Smart SEO Meta Tags - Auto-detect Post Pages for Social Sharing --}}
     @php
